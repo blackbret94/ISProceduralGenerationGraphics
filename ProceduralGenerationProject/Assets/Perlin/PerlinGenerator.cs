9f28548acb05c8fdf73279be98ea5065a7e5251a
Bret <blackb@lafayette.edu>
2015-09-06 00:40:57 -0400

    Initial commit, includes a perlin noise example

---
+++ ProceduralGenerationProject/Assets/Perlin/PerlinGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This simple script fills a terrain with perlin noise.  This is the most simple form of procedural generation
+// I will be coding and was completed primarily to become familiar with Unity's terrain API
+
+public class PerlinGenerator : MonoBehaviour {
+	Terrain terrain;
+
+	// Use this for initialization
+	void Start () {
+		// get size of terrain
+		terrain = (Terrain)gameObject.GetComponent ("Terrain");
+		Vector3 tSize = terrain.terrainData.size;
+		Debug.Log (tSize);
+
+		// set perlin noise origin coordinates
+		float xOrg = Random.Range (0, .1f);
+		float yOrg = Random.Range (0, .1f);
+
+		// generate random values
+		float[,] heightmap = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+
+		// fill array with perlin noise values
+		for(int i=0;i<heightmap.GetLength(0);i++){
+			for(int j=0;j<heightmap.GetLength(1);j++){
+				float xCoord = xOrg + (float)i / ((float)heightmap.GetLength(0)/5);
+				float yCoord = yOrg + (float)j / ((float)heightmap.GetLength(1)/5);
+				heightmap[i,j] = Mathf.PerlinNoise(xCoord, yCoord);
+			}
+		}
+
+		// reattatch array to terrain
+		terrain.terrainData.SetHeights(0,0,heightmap);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ISProceduralGenerationGraphics
-A repo representing my independent study of procedurally generated graphics
+A repo representing my independent study of procedurally generated graphics.
+
+Several forms of procedural generation will be covered here.  Areas of intended study include landscapes, cities, dungeons, RPG quests, and plotlines.
+
+The first three areas are related to terrain, and will be grouped as such.  The terrain will be coded using Unity3D.  The last two are related to storytelling and will likely be coded in Python.
