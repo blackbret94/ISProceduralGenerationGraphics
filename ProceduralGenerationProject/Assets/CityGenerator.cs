587f202bafa22036d36f59f9d7b49c216262f13a
Bret <blackb@lafayette.edu>
2015-10-06 01:57:26 -0400

    Started islands generator

---
+++ ProceduralGenerationProject/Assets/CityGenerator.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using csDelaunay;
+
+// splits the terrain into a set of Voronoi polygons, creates a path through the graph, and raises islands that are assigned biomes
+// parts of this code has been adapted from http://forum.unity3d.com/threads/delaunay-voronoi-diagram-library-for-unity.248962/
+
+public class CityGenerator : MonoBehaviour {
+	public int minPolygons; // minimum number of polygons
+	public int maxPolygons; // maximum number of polygons
+	public int seed = 0;
+	Terrain terrain; // the actual terrain
+	float[,] pointArray; // array representing point values
+	float[,] heightmap; // array representing heightmap
+
+	// The number of polygons/sites we want
+	public int polygonNumber = 0;
+	
+	// This is where we will store the resulting data
+	private Dictionary<Vector2f, Site> sites;
+	private List<Edge> edges;
+	
+	// Use this for initialization
+	void Start () {
+		// set seed
+		Random.seed = seed;
+
+		// pick number of polygons
+		polygonNumber = Random.Range (minPolygons, maxPolygons);
+		
+		// get terrain
+		terrain = (Terrain)gameObject.GetComponent ("Terrain");
+		
+		// get heightmap
+		heightmap = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+		
+		// initialize heightmap array
+		for(int i=0;i<heightmap.GetLength(0);i++){
+			for(int j=0;j<heightmap.GetLength(1);j++){
+				//heightmap[i,j] = 0f;
+				heightmap[i,j] = 20f/terrain.terrainData.size.y;
+			}
+		}
+
+		// Create your sites (lets call that the center of your polygons)
+		List<Vector2f> points = CreateRandomPoint();
+		
+		// Create the bounds of the voronoi diagram
+		// Use Rectf instead of Rect; it's a struct just like Rect and does pretty much the same,
+		// but like that it allows you to run the delaunay library outside of unity (which mean also in another tread)
+		Rectf bounds = new Rectf(0,0,512,512);
+		
+		// There is a two ways you can create the voronoi diagram: with or without the lloyd relaxation
+		// Here I used it with 2 iterations of the lloyd relaxation
+		Voronoi voronoi = new Voronoi(points,bounds,5);
+		
+		// But you could also create it without lloyd relaxtion and call that function later if you want
+		//Voronoi voronoi = new Voronoi(points,bounds);
+		//voronoi.LloydRelaxation(5);
+		
+		// Now retreive the edges from it, and the new sites position if you used lloyd relaxtion
+		sites = voronoi.SitesIndexedByLocation;
+		edges = voronoi.Edges;
+
+		// apply polygons
+		DisplayVoronoiDiagram();
+		
+		// reattatch array to terrain
+		terrain.terrainData.SetHeights(0,0,heightmap);
+		
+	}
+	
+	// choose seeds for the polygons
+	// adaption from http://forum.unity3d.com/threads/delaunay-voronoi-diagram-library-for-unity.248962/
+	private List<Vector2f> CreateRandomPoint() {
+		// Use Vector2f, instead of Vector2
+		// Vector2f is pretty much the same than Vector2, but like you could run Voronoi in another thread
+		List<Vector2f> points = new List<Vector2f>();
+		for (int i = 0; i < polygonNumber; i++) {
+			points.Add(new Vector2f(Random.Range(0,terrain.terrainData.heightmapWidth), Random.Range(0,terrain.terrainData.heightmapHeight)));
+		}
+		
+		return points;
+	}
+
+	// Here is a very simple way to display the result using a simple bresenham line algorithm
+	// Just attach this script to a quad
+	// adaption from http://forum.unity3d.com/threads/delaunay-voronoi-diagram-library-for-unity.248962/
+	private void DisplayVoronoiDiagram() {
+		//Texture2D tx = new Texture2D(512,512);
+		//foreach (KeyValuePair<Vector2f,Site> kv in sites) {
+		//	tx.SetPixel((int)kv.Key.x, (int)kv.Key.y, Color.red);
+		//}
+		foreach (Edge edge in edges) {
+			// if the edge doesn't have clippedEnds, if was not within the bounds, dont draw it
+			if (edge.ClippedEnds == null) continue;
+			
+			DrawLine(edge.ClippedEnds[LR.LEFT], edge.ClippedEnds[LR.RIGHT]);
+		}
+		//tx.Apply();
+		
+		//this.renderer.material.mainTexture = tx;
+	}
+	
+	// Bresenham line algorithm
+	// adaption from http://forum.unity3d.com/threads/delaunay-voronoi-diagram-library-for-unity.248962/
+	private void DrawLine(Vector2f p0, Vector2f p1, int offset = 0) {
+		int x0 = (int)p0.x;
+		int y0 = (int)p0.y;
+		int x1 = (int)p1.x;
+		int y1 = (int)p1.y;
+		
+		int dx = Mathf.Abs(x1-x0);
+		int dy = Mathf.Abs(y1-y0);
+		int sx = x0 < x1 ? 1 : -1;
+		int sy = y0 < y1 ? 1 : -1;
+		int err = dx-dy;
+		
+		while (true) {
+			//tx.SetPixel(x0+offset,y0+offset,c);
+			//heightmap[x0+offset,y0+offset] = 0f;
+			ChangeHeightReg(x0+offset,y0+offset,5,0f);
+
+			if (x0 == x1 && y0 == y1) break;
+			int e2 = 2*err;
+			if (e2 > -dy) {
+				err -= dy;
+				x0 += sx;
+			}
+			if (e2 < dx) {
+				err += dx;
+				y0 += sy;
+			}
+		}
+	}
+
+	// modifies a size x size grid
+	private void ChangeHeightReg(int x,int y, int size, float height){
+		int tw = terrain.terrainData.heightmapWidth;
+		int th = terrain.terrainData.heightmapHeight;
+
+		for (int i = x-(size-1)/2; i<x+(size-1)/2; i++) {
+			for (int j = y-(size-1)/2; j<y+(size-1)/2; j++) {
+				if(i>=0 && i< tw && j>=0 && j<th){
+					heightmap[i,j] = height;
+				}
+			}
+		}
+	}
+
+}
---
+++ ProceduralGenerationProject/Assets/Island Generator/CoastlineGenerator.cs
@@ -0,0 +1,505 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this script generates a coastline by raising the land slightly
+// inspired by the algorithm suggested by in Controlled Procedural
+// Terrain Generation Using Software Agents
+
+public class CoastlineGenerator : MonoBehaviour {
+	public float startTokens; // how big is the land mass?
+	public float limit; // how many tokens are allowed to each agent
+	public float agentRange = 10f; // how far can the attractor and repulsor be? Must be greater than 1.
+	public float smoothReturnChance = 50f; // how regularly do smoothing agents return to the start?
+	public float smoothAgents = 100f; // how many smoothing agents are there?
+	public int seed = 0;
+	public GameObject tree01;
+	Terrain terrain; // the actual terrain
+	float[,] pointArray; // array representing point values
+	float[,] heightmap; // array representing heightmap
+
+	// agents do the work
+	public struct Agent {
+		public Vector3 point; // location of the agent
+		public float tokens; // the number of verticies it is responsible for
+		public Vector3 direction; // the direction it goes
+
+
+		// constructor
+		public Agent(Vector3 p,float t, Vector3 d){
+			point = p;
+			tokens = t;
+			direction = d;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		// set seed
+		Random.seed = seed;
+
+		// get terrain
+		terrain = (Terrain)gameObject.GetComponent ("Terrain");
+		
+		// get heightmap
+		heightmap = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+
+		// create array to hold point values.  Points with the highest values will be raised
+		pointArray = new float[3,3];
+
+		// initialize heightmap array
+		for(int i=0;i<heightmap.GetLength(0);i++){
+			for(int j=0;j<heightmap.GetLength(1);j++){
+				heightmap[i,j] = 0f;
+			}
+		}
+
+		// create first agent
+		Agent firstAgent = new Agent (new Vector3 (terrain.terrainData.heightmapWidth / 2, 0, terrain.terrainData.heightmapHeight / 2), startTokens, RandomDirection ());
+
+		// run coastline generation
+		CoastlineGenerate (firstAgent);
+
+		// smooth
+		//heightmap = SmoothTerrain (heightmap);
+
+		// add noise
+		heightmap = PerlinNoise (heightmap);
+
+		// smooth
+		heightmap = SmoothTerrain (heightmap);
+		
+		// reattatch array to terrain
+		terrain.terrainData.SetHeights(0,0,heightmap);
+
+		// generate enviornment
+		GenerateNature (heightmap);
+
+		// generate textures
+		GenerateTexture ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// this is the method agents use to do their work
+	void CoastlineGenerate(Agent agent){
+		if (agent.tokens >= limit) {
+			// create 2 child agents
+			for (int i=0; i<2; i++) {
+				// point
+				Vector3 newPoint = RandomAdjacentPoint (agent.point);//+Random.Range(0,2)*agent.direction;
+
+				// direction
+				Vector3 newDir = RandomDirection ();
+				//Debug.Log(newDir);
+
+				// create agent
+				Agent newAgent = new Agent (newPoint, Mathf.Floor (agent.tokens / 2), newDir);
+
+				// run recursively
+				CoastlineGenerate (newAgent);
+			}
+		} else { 
+			// for each token
+			for(int i=0;i<agent.tokens;i++){
+				// check if the point is surrounded by land
+				while(IsSurroundedByLand(agent.point)){
+					agent.point = agent.point+agent.direction;
+				}
+
+				// create repulsor
+				Vector3 repulDirection = RandomDirection();
+				Vector3 repul = new Vector3(agent.point.x,agent.point.y,agent.point.z); // clone
+				repul = repul+repulDirection*Random.Range(1f,agentRange);// translate
+				
+				// create attractor
+				Vector3 attraDirection = repulDirection*-1;//RandomDirection();
+				//while(attraDirection == repulDirection){
+				//	attraDirection = RandomDirection();
+				//}
+
+				Vector3 attra = new Vector3(agent.point.x,agent.point.y,agent.point.z); // clone
+				attra = attra+attraDirection*Random.Range(1f,agentRange);// translate
+
+				// for all points surrounding the random adjacent point, score the point
+				for(float j=0f;j<3f;j++){
+					for(float k=0f;k<3f;k++){
+						//  score if not the actual point
+						if(j!=1f&&k!=1f){
+							Vector3 thisPoint = new Vector3(agent.point.x+j-1,0,agent.point.z+k-1);
+							pointArray[(int)j,(int)k] = ScorePoint(thisPoint,repul,attra);
+							//pointArray[(int)j,(int)k] = ScorePoint(agent.point,repul,attra);
+						}
+					}
+				}
+
+				// raise the highest scoring surrounding point
+				// save pointArray coordinates of highest scoring point
+				int hX = 0;
+				int hZ = 0;
+
+				// save 0,0 point
+				int sX = (int)agent.point.x-1;
+				int sZ = (int)agent.point.z-1;
+				
+				// find the highest point
+				for(int j=0;j<3;j++){
+					for(int k=0;k<3;k++){
+						// check that it is higher than the previously saved point
+						if(pointArray[j,k]>pointArray[hX,hZ]){
+							hX = j;
+							hZ = k;
+						}
+					}
+				}
+				
+				// raise the heightmap of the heightest scoring point
+				heightmap[sX+hX,sZ+hZ] = 2f/terrain.terrainData.size.y;
+
+				// move to this point
+				agent.point = new Vector3(sX+hX,0,sZ+hZ);
+			}
+		}
+	}
+
+	// returns a random point surrounding the initial point
+	// if the point is out of bounds it returns the center of the map
+	Vector3 RandomAdjacentPoint(Vector3 StartPoint){
+		// get random value
+		int rand = (int)Mathf.Round (Random.Range (0f, 7f));
+		Vector3 newVector = new Vector3(1,0,1);
+		switch (rand) {
+			//-1,-1
+			case 0:
+			newVector = new Vector3(StartPoint.x-1,0,StartPoint.z-1);
+			break;
+
+			//0,-1
+		case 1:
+			newVector = new Vector3(StartPoint.x,0,StartPoint.z-1);
+			break;
+
+			//1,-1
+		case 2:
+			newVector = new Vector3(StartPoint.x+1,0,StartPoint.z-1);
+			break;
+
+			//-1,0
+		case 3:
+			newVector = new Vector3(StartPoint.x-1,0,StartPoint.z);
+			break;
+
+			//1,0
+		case 4:
+			newVector = new Vector3(StartPoint.x+1,0,StartPoint.z);
+			break;
+
+			//-1,1
+		case 5:
+			newVector = new Vector3(StartPoint.x-1,0,StartPoint.z+1);
+			break;
+
+			//0,1
+		case 6:
+			newVector = new Vector3(StartPoint.x,0,StartPoint.z+1);
+			break;
+
+			//1,1
+		case 7:
+			newVector = new Vector3(StartPoint.x+1,0,StartPoint.z+1);
+			break;
+
+		}
+
+		if (newVector.x < 0 || newVector.x > terrain.terrainData.heightmapWidth || newVector.z < 0 || newVector.z > terrain.terrainData.heightmapHeight) {
+			newVector.x = terrain.terrainData.heightmapWidth/2;
+			newVector.z = terrain.terrainData.heightmapHeight/2;
+		}
+
+		// return
+		return newVector;
+	}
+
+	// returns a random direction on the XZ plane
+	Vector3 RandomDirection(){
+		float xDir;
+		float zDir;
+
+		// repeat, to make sure there IS a direction
+		do{
+			xDir = Random.Range (-1f, 1f);
+			zDir = Random.Range (-1f, 1f);
+		} while(xDir == 0f && zDir == 0f);
+
+		return new Vector3(xDir,0,zDir);
+	}
+
+	// checks to make sure the surrounding landmass is not raised
+	bool IsSurroundedByLand(Vector3 pt){
+		// save 0,0
+		int sX = (int)pt.x-1;
+		int sZ = (int)pt.z-1;
+
+		// check for out of bounds
+		if (sX < 0 || sZ < 0 || sX + 2 > terrain.terrainData.heightmapWidth || sZ + 2 > terrain.terrainData.heightmapHeight) {
+			return false;
+		}
+
+		// iterate through adjacent points
+		for(int j=0;j<3;j++){
+			for(int k=0;k<3;k++){
+				if(heightmap[sX+j,sZ+k]==0){
+					// this point is sea level
+					return false;
+				}
+			}
+		}
+
+		// no points were at sea level
+		return true;
+	}
+	
+	// scores a point
+	float ScorePoint(Vector3 pt, Vector3 repulsor, Vector3 attractor){
+		// square of distance between pt and attractor
+		float da = Vector3.Distance (pt, attractor);
+		da = da * da;
+
+		// square of distance between pt and repulsor
+		float dr = Vector3.Distance (pt, repulsor);
+		dr = dr * dr;
+
+		// vertical distance to the edge of the map
+		float deV = Mathf.Min(Vector3.Distance(pt,new Vector3(pt.x,0,0)),Vector3.Distance(pt,new Vector3(pt.x,0,terrain.terrainData.heightmapHeight)));
+
+		// horizontal distance to the edge of the map;
+		float deH = Mathf.Min(Vector3.Distance(pt,new Vector3(0,0,pt.z)),Vector3.Distance(pt,new Vector3(terrain.terrainData.heightmapWidth,0,pt.z)));
+
+		// choose the best of the vert and horiz distance and set it to the distance to the edge of the map
+		float de = Mathf.Min(deV,deH);
+
+		// corners of the map
+		/*for(int i=0;i<=1;i++){
+			for(int j=0;j<=1;j++){
+				de = Mathf.Min(de,Vector3.Distance(pt,new Vector3(i*terrain.terrainData.heightmapWidth,0,j*terrain.terrainData.heightmapHeight)));
+			}
+		}*/
+
+
+		de = de * de;
+
+		// return the calculation
+		return dr - da;//+3*de;
+	}
+
+	// uses perlin noise to raise the terrain
+	float[,] PerlinNoise(float[,] hm){
+		// fractal origins
+		float xOrg = Random.Range (0, .1f);
+		float yOrg = Random.Range (0, .1f);
+
+		// iterate through maps
+		for(int i=0;i<hm.GetLength(0);i++){
+			for(int j=0;j<hm.GetLength(1);j++){
+				// make sure this is part of the land mass
+				if(hm[i,j] > 0){
+					// get perlin noise coordinates
+					float xCoord = xOrg + (float)i / ((float)hm.GetLength(0)/25);
+					float yCoord = yOrg + (float)j / ((float)hm.GetLength(1)/25);
+
+					// multiply the current height by a multiple of the perlin noise
+					hm[i,j] = hm[i,j]+3*Mathf.PerlinNoise(xCoord, yCoord)/80;
+				}
+			}
+		}
+
+		// return
+		return heightmap;
+	}
+
+	// smooths the terrain by randomly walking around the landmass
+	float[,] SmoothTerrain(float[,] hm){
+		// for each agent...
+		for (int j=0; j<smoothAgents; j++) {
+			// create starting point and make sure starting point is on the landmass.
+			Vector3 startingPoint = new Vector3 (terrain.terrainData.heightmapWidth / 2, 0, terrain.terrainData.heightmapHeight / 2);
+			/*do {
+				startingPoint = new Vector3 (Random.Range(1,terrain.terrainData.heightmapWidth-1), 0, Random.Range(1,terrain.terrainData.heightmapHeight-1));
+			} while(hm[(int)startingPoint.x,(int)startingPoint.z] == 0);*/
+
+			// clone starting point
+			Vector3 activePoint = new Vector3 (startingPoint.x, 0, startingPoint.z);
+
+			for (int i=0; i<startTokens; i++) {
+				// weight average
+				float wAvg = GetVonNeumannAverage (hm, activePoint);
+
+				// set height
+				hm [(int)activePoint.x, (int)activePoint.z] = wAvg;
+
+				// chance to return to start
+				if (Mathf.Round (Random.Range (0, smoothReturnChance)) == 0) {
+					activePoint.x = startingPoint.x;
+					activePoint.z = startingPoint.z;
+				} else {
+					// move to neighboring point
+					activePoint = RandomAdjacentPoint (activePoint);
+				}
+			}
+		}
+
+		// return
+		return hm;
+	}
+
+	// returns a weighted average of the extended von Neumann neighborhood
+	float GetVonNeumannAverage(float[,] hm, Vector3 point){
+		// declare average
+		float avg = 0;
+
+		// get horizontal sum
+		for(int i=-2;i<=2;i++){
+			if(point.x+i>0 && point.x+i<terrain.terrainData.heightmapWidth){
+				avg += hm[(int)point.x+i,(int)point.z];
+			}
+		}
+
+		// get verticle sum
+		for (int i=-2; i<=2; i++) {
+			if(point.z+i>0 && point.z+i<terrain.terrainData.heightmapHeight){
+				avg += hm[(int)point.x,(int)point.z+i];
+			}
+		}
+
+		// add center value (making the pointed weighted 3x the value of the other points)
+		avg += hm[(int)point.x,(int)point.z];
+
+		// divide by 11
+		if (avg > 0) {
+			avg = avg / 11;
+		}
+
+		// return
+		return avg;
+	}
+
+	// generates nautre
+	void GenerateNature(float[,] hm){
+		// wipe old trees
+		terrain.terrainData.treeInstances = new TreeInstance[0];
+
+		// iterate through vertecies
+		for(int i=0;i<terrain.terrainData.heightmapWidth;i++){
+			for(int j=0;j<terrain.terrainData.heightmapHeight;j++){
+				// TREES
+				// is it above sea level?
+				if(terrain.terrainData.GetHeight(i,j) > 1){
+					// is it relatively flat?
+					Vector3 thisPoint = new Vector3(i,0,j);
+					float vna = GetVonNeumannAverage(hm,thisPoint);
+
+					if(vna/hm[i,j]< 1.5 && hm[i,j]/vna < 1.5){
+						// chance to generate tree
+						if(Mathf.Round(Random.Range(0,15))==0){
+							// generate trees
+							TreeInstance treeInst = new TreeInstance();
+							treeInst.prototypeIndex = Random.Range(0,2);
+							Vector3 position = new Vector3(((float)i)/terrain.terrainData.heightmapWidth,0,((float)j)/terrain.terrainData.heightmapHeight);
+							//Vector3 position = new Vector3(i,0,j);
+							position.y = terrain.terrainData.GetInterpolatedHeight((float)i/terrain.terrainData.heightmapWidth, (float)j/terrain.terrainData.heightmapHeight) / terrain.terrainData.size.y;
+							treeInst.position = position;
+							float sizeMod = Random.Range(.8f,1.2f);
+							treeInst.heightScale = sizeMod;
+							treeInst.widthScale = sizeMod;
+
+							float colorMod = Random.Range(.8f,1f);
+							treeInst.color = new Color (colorMod, colorMod, colorMod);
+							treeInst.lightmapColor = new Color (1, 1, 1);
+							terrain.AddTreeInstance(treeInst);
+
+							//print(position);
+
+						}
+					}
+
+				}
+			}
+		}
+
+		// flush and print
+		terrain.Flush();
+		print(terrain.terrainData.treeInstances.Length); //does show trees are being added to the treeInstances array
+	}
+
+	// add textures
+	// ASSUMES 0-Grass 1-Sand 2-Rock
+	void GenerateTexture(){
+		// get data and create array for textures
+		TerrainData td = terrain.terrainData;
+		float[, ,] textureData = new float[td.alphamapWidth, td.alphamapHeight, td.alphamapLayers];
+
+		//td.alphamapHeight = td.heightmapHeight;
+		//td.alphamapWidth = td.heightmapWidth;
+		Debug.Log (td.alphamapHeight);
+		Debug.Log (td.alphamapResolution);
+		Debug.Log (td.heightmapResolution);
+		float resMod = td.alphamapHeight/td.heightmapHeight;// adjusts the alphamap coords to the heightmap coords
+		Debug.Log (resMod);
+
+		// assign textures
+		for (int i=0; i<td.alphamapWidth; i++) {
+			for(int j=0; j<td.alphamapHeight;j++){
+				// set grass default
+				Vector3 splat = new Vector3(1,0,0);
+				//textureData[i,j,0] = 1;
+
+				// draw sand if height < 1.5
+				if(td.GetInterpolatedHeight((float)i/td.heightmapWidth,(float)j/td.heightmapHeight) <= 1){
+					//textureData[i,j,1] = 1;
+					splat.y = 1;
+				} else if(td.GetInterpolatedHeight((float)i/td.heightmapWidth,(float)j/td.heightmapHeight) <= 2){
+					splat.y = .5f;
+				}
+
+				// apply rock if the slope is steep
+				if(td.GetSteepness((float)i/td.heightmapWidth,(float)j/td.heightmapHeight) > 35){
+					//textureData[i,j,2] = 1;
+					splat.z = 1;
+					//splat.x = 0;
+					//splat.y = 0;
+					//Debug.Log(td.GetSteepness((float)i/td.heightmapWidth,(float)j/td.heightmapHeight));
+				}
+
+				// normalize
+				splat.Normalize();
+                textureData[i, j, 0] = splat.x;
+                textureData[i, j, 1] = splat.y;
+                textureData[i, j, 2] = splat.z;
+			}
+		}
+		// invert array
+		textureData = RotateMap90 (textureData, td.alphamapWidth);
+		//textureData = RotateMap90 (textureData, td.alphamapWidth);
+
+		// apply
+		td.SetAlphamaps (0, 0, textureData);
+	}
+
+	// helper function to rotate a matrix, inspired by http://stackoverflow.com/questions/42519/how-do-you-rotate-a-two-dimensional-array
+	// rotates 90 degrees
+	static float[,,] RotateMap90(float[,,] matrix, int n) {
+		float[,,] ret = new float[n, n, 3];
+		
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				ret[i, j, 0] = matrix[j, i, 0];
+				ret[i, j, 1] = matrix[j, i, 1];
+				ret[i, j, 2] = matrix[j, i, 2];
+			}
+		}
+		
+		return ret;
+	}
+}
